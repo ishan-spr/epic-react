56ee548955f7ef5847d5785df153da6b75afa291
ishan-spr <ishan.chaudhary@sprinklr.com>
2022-08-24 17:16:39 +0530

    First Exercise done

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,9 +1,80 @@
 // 🐨 you'll need to import react and createRoot from react-dom up here
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+import React from "react";
+import { createRoot } from "react-dom"
+import { Logo } from "components/logo";
+import '@reach/dialog/styles.css'
+import Dialog from "@reach/dialog";
+import { VisuallyHidden } from "@reach/visually-hidden";
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
 
-// 🐨 use createRoot to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+function LoginForm({ onSubmit, buttonText }) {
+    function handleSubmit(event) {
+        event.preventDefault()
+        const { username, password } = event.target.elements
+
+        onSubmit({
+            username: username.value,
+            password: password.value,
+        })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">Username</label>
+                <input id="username" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input id="password" type="password" />
+            </div>
+            <div>
+                <button type="submit">{buttonText}</button>
+            </div>
+        </form>
+    )
+}
+
+function App() {
+    const [openLogin, setOpenlogin] = React.useState(false)
+    const [openegister, setOpenRegister] = React.useState(false)
+
+    const loginClose = () => (setOpenlogin(false))
+    const registerClose = () => (setOpenRegister(false))
+
+    function login(formData) {
+        console.log('login', formData)
+    }
+
+    function register(formData) {
+        console.log('register', formData)
+    }
+
+    return (
+        <>
+            <Logo />
+            <h1>Bookshelf</h1>
+            <button onClick={() => (setOpenlogin(true))}>Login</button>
+            <button onClick={() => (setOpenRegister(true))}>Register</button>
+            <Dialog isOpen={openLogin} onDismiss={loginClose}>
+                <button className="close-button" onClick={loginClose}>
+                    <VisuallyHidden>Close</VisuallyHidden>
+                    <span aria-hidden>×</span>
+                </button>
+                <LoginForm onSubmit={login} buttonText={"Login"} />
+            </Dialog>
+            <Dialog isOpen={openegister} onDismiss={registerClose}>
+                <button className="close-button" onClick={registerClose}>
+                    <VisuallyHidden>Close</VisuallyHidden>
+                    <span aria-hidden>×</span>
+                </button>
+                <LoginForm onSubmit={register} buttonText={"Register"} />
+            </Dialog>
+        </>
+    )
+}
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
+export { root }
