63e4efbd820d47a610064bb6710606ef1bc08b2f
ishan-spr <ishan.chaudhary@sprinklr.com>
2022-08-29 15:43:53 +0530

    Auth completed

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,28 +1,41 @@
 /** @jsx jsx */
-import {jsx} from '@emotion/core'
+import { jsx } from '@emotion/core'
 
 import * as React from 'react'
-// 🐨 you're going to need this:
-// import * as auth from 'auth-provider'
-import {AuthenticatedApp} from './authenticated-app'
-import {UnauthenticatedApp} from './unauthenticated-app'
+import * as auth from 'auth-provider'
+import { AuthenticatedApp } from './authenticated-app'
+import { UnauthenticatedApp } from './unauthenticated-app'
+import { client } from './utils/api-client'
+import { useAsync } from './utils/hooks'
+import { FullPageSpinner } from './components/lib'
 
-function App() {
-  // 🐨 useState for the user
+async function getUser() {
+  let user = null
 
-  // 🐨 create a login function that calls auth.login then sets the user
-  // 💰 const login = form => auth.login(form).then(u => setUser(u))
-  // 🐨 create a registration function that does the same as login except for register
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', { token })
+    user = data.user
+  }
 
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
-
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
-
-  return <UnauthenticatedApp />
+  return user
 }
 
-export {App}
+function App() {
+  const { data: user, error, isLoading, isIdle, isError, run, setData } = useAsync()
+  const login = form => auth.login(form).then(u => setData(u))
+  const register = form => auth.register(form).then(u => setData(u))
+  const logout = () => auth.logout().then(() => setData(null))
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  return (
+    isIdle || isLoading ? <FullPageSpinner /> : user ? <AuthenticatedApp user={user} logout={logout} /> : <UnauthenticatedApp login={login} register={register} />
+  )
+}
+export { App }
 
 /*
 eslint
